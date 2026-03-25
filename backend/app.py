413b82f6c6addbb5c49493fe08f121fbc771f1d5
Kingston Whyte <kingstonwhyte7@gmail.com>
2026-03-25 14:32:13 +0000

    basic started

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,20 @@
+import uvicorn
+import yaml
+
+from fastapi import FastAPI
+
+
+with open("config.yml") as f:
+    config = yaml.safe_load(f)
+
+app = FastAPI()
+
+@app.post(
+    "/api/user"
+)
+
 def main():
-    print("Hello from backend!")
+    uvicorn.run(**config.get("uvicorn"))
 
 
 if __name__ == "__main__":
