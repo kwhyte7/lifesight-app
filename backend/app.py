adfcbb89c4a6ec119637e008b65ae01c24b1d5d5
Kingston Whyte <kingstonwhyte7@gmail.com>
2026-03-25 14:36:35 +0000

    some basic stuff written

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,29 @@
 import uvicorn
 import yaml
 
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, HTTPException, Request, status
 
 with open("config.yml") as f:
     config = yaml.safe_load(f)
 
 app = FastAPI()
 
+# i'd like JWT authentication - don't know how yet.
+
 @app.post(
-    "/api/user"
+    "/api/user",
+    #response_model=
+    status_code=status.HTTP_201_CREATED,
 )
+def create_user(create_user: schemas):# create user
+    pass
+
+@app.get(
+    "/api/user/{user_id}"
+    # response_model=
+)
+def get_user(): # you probably want public and private users.
+    pass
 
 def main():
     uvicorn.run(**config.get("uvicorn"))
