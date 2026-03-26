80fb3ff6f5ddd8c4cad42e5b04af7ebee39c16bf
Kingston Whyte <kingstonwhyte7@gmail.com>
2026-03-26 15:29:32 +0000

    updated schemas

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./mock.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread" : False}, # for sqlite
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    with SessionLocal() as db:
+        yield db
---
+++ backend/schemas.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+class UserBase(BaseModel):
+    id: int
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr # need email for resetting passwords
+
+class MembershipBase(BaseModel):
+    id: int
+    owner_id: int
+    is_free_trial: bool
+    created_at: datetime
+
+class MembershipUsersBase(BaseModel):
+    membership_id: int
+    user_id: int
+    created_at: datetime
+
+class DayBase(BaseModel):
+    id: int
+    user_id: int
+    created_at: datetime
+
+class DayFoodBase(BaseModel):
+    day_id: int
+    food_id: int
+    created_at: datetime
+
+class FoodBase(BaseModel):
+    id: int
+    user_id: int
+    name: str
+    image_name: str
+    image_path: str
+    nutritional_information_id: str
+    quantity: str
+    created_at: datetime
+
+class NutritionalInformationBase(BaseModel):
+    id: int
+    per_amount: int
+    per_amount_measurement: int
+    energy: int
+    fat: int
+    of_which_saturates: int
+    carbohydrates: int
+    of_which_sugars: int
+    fibre: int
+    protien: int
+    salt: int
+    created_at: int
+
+class FoodIngredientBase(BaseModel):
+    food_id: int 
+    ingredient_id: int
+    created_at: datetime
+
+class IngredientNameIngredientBase(BaseModel):
+    ingredient_name: str
+    ingrendient_id: int
+    created_at: datetime
+
+class IngredientBase(BaseModel):
+    id: int
+    standardised_name: str
+    description_from_web: str
+    key_dangers: str
+    key_negatives: str
+    key_benefits: str
+    created_at: datetime
+
+class IngredientWebSourceBase(BaseModel):
+    ingredient_id: int
+    source_id: int
+    created_at: datetime
+
+class WebSourceBase(BaseModel):
+    id: int
+    title: str
+    author: str
+    url: str
+    description: str
+    created_at: datetime
